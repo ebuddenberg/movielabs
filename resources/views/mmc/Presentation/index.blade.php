e4fc94bf9f9aab5acf90652ce4d794c63bf75d02
ebuddenberg <buddenbergev@gmail.com>
2022-04-03 18:56:22 +0800

    mmc temmplate is updated

--- resources/views/mmc/Presentation/index.blade.php
+++ resources/views/mmc/Presentation/index.blade.php
@@ -1,37 +1,44 @@
 @php
-    $Presentation = $data['Presentations'];
+    $Presentations = $data['Presentations'];
 @endphp
 <manifest:Presentations>
-        <manifest:Presentation PresentationID="{{$Presentation['Presentation']['@PresentationID']}}">
+    @foreach($Presentations as $Presentation)
+        <manifest:Presentation PresentationID="{{$Presentation['@PresentationID']}}">
             <manifest:TrackMetadata>
-            @isset($Presentation['Presentation']['TrackMetadata'])
-
+            @isset($Presentation['TrackMetadata'])
                 @php
-                $TackMetadata = $Presentation['Presentation']['TrackMetadata'];
+                $TackMetadata = $Presentation['TrackMetadata'];
                 @endphp
 
                 @isset($TackMetadata['TrackSelectionNumber'])
                     <manifest:TrackSelectionNumber>{{$TackMetadata['TrackSelectionNumber']}}</manifest:TrackSelectionNumber>
                 @endisset
 
-                @isset($TrackMeta['VideoTrackReference'])
-                    <manifest:VideoTrackReference>
-                        <manifest:VideoTrackID>{{$TackMetadata['VideoTrackReference']['VideoTrackID']}}</manifest:VideoTrackID>
-                    </manifest:VideoTrackReference>
+                @isset($TrackMeta['VideoTrackReferences'])
+                    @foreach($TrackMeta['VideoTrackReferences'] as $VideoTrackReference)
+                        <manifest:VideoTrackReference>
+                            <manifest:VideoTrackID>{{$VideoTrackReference['VideoTrackID']}}</manifest:VideoTrackID>
+                        </manifest:VideoTrackReference>
+                    @endforeach
                 @endisset
 
-                @isset($TrackMeta['AudioTrackReference'])
+                @isset($TrackMeta['AudioTrackReferences'])
+                    @foreach($TrackMeta['AudioTrackReferences'] as $AudioTrackReference)
                     <manifest:AudioTrackReference>
-                        <manifest:AudioTrackID>{{$TackMetadata['AudioTrackReference']['AudioTrackID']}}</manifest:AudioTrackID>
+                        <manifest:AudioTrackID>{{$AudioTrackReference['AudioTrackID']}}</manifest:AudioTrackID>
                     </manifest:AudioTrackReference>
+                    @endforeach
                 @endisset
                 
-                @isset($TrackMeta['SubtitleTrackReference'])
-                    <manifest:SubtitleTrackReference>
-                        <manifest:SubtitleTrackID>{{$TackMetadata['SubtitleTrackReference']['SubtitleTrackID']}}</manifest:SubtitleTrackID>
-                    </manifest:SubtitleTrackReference>
+                @isset($TrackMeta['SubtitleTrackReferences'])
+                    @foreach($TrackMeta['SubtitleTrackReferences'] as $SubtitleTrackReference)
+                        <manifest:SubtitleTrackReference>
+                            <manifest:SubtitleTrackID>{{$SubtitleTrackReference['SubtitleTrackID']}}</manifest:SubtitleTrackID>
+                        </manifest:SubtitleTrackReference>
+                    @endforeach
                 @endisset
             </manifest:TrackMetadata>
             @endisset
         </manifest:Presentation>
+    @endforeach
 </manifest:Presentations>
