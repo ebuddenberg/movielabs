f79abc1927733f8b76a6dc26028fd11216e68822
ebuddenberg <buddenbergev@gmail.com>
2022-05-19 16:15:16 +0800

    remove indent

--- resources/views/mec/ReleaseHistory/index.blade.php
+++ resources/views/mec/ReleaseHistory/index.blade.php
@@ -9,16 +9,16 @@
 @endisset
 
 @php
-    $ReleaseHistory = $data['BasicMetadata-type']['ReleaseHistory'];
+$ReleaseHistory = $data['BasicMetadata-type']['ReleaseHistory'];
 @endphp
 <md:ReleaseHistory>
-    <md:ReleaseType>{{$ReleaseHistory['ReleaseType']}}</md:ReleaseType>
-    @isset($ReleaseHistory['DistrTerritory'])
-    <md:DistrTerritory>
-        <md:country>{{$ReleaseHistory['DistrTerritory']['country']}}</md:country>
-    </md:DistrTerritory>
-    @endisset
-    @isset($ReleaseHistory['Date'])
-    <md:Date>{{$ReleaseHistory['Date']}}</md:Date>
-    @endisset
+<md:ReleaseType>{{$ReleaseHistory['ReleaseType']}}</md:ReleaseType>
+@isset($ReleaseHistory['DistrTerritory'])
+<md:DistrTerritory>
+<md:country>{{$ReleaseHistory['DistrTerritory']['country']}}</md:country>
+</md:DistrTerritory>
+@endisset
+@isset($ReleaseHistory['Date'])
+<md:Date>{{$ReleaseHistory['Date']}}</md:Date>
+@endisset
 </md:ReleaseHistory>
